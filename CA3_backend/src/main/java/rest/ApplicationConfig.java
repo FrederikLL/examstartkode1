71cbbe2ccfca82baf68cf46ee9885f427c276f57
Frederik Løwe Lassen <frizz97@hotmail.com>
2020-01-13 18:47:40 +0100

    ny klasse

--- CA3_backend/src/main/java/rest/ApplicationConfig.java
+++ CA3_backend/src/main/java/rest/ApplicationConfig.java
@@ -24,6 +24,7 @@ public class ApplicationConfig extends Application {
         resources.add(cors.CorsResponseFilter.class);
         resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(rest.DemoResource.class);
+        resources.add(rest.SomethingResource.class);
         resources.add(security.JWTAuthenticationFilter.class);
         resources.add(security.LoginEndpoint.class);
         resources.add(security.RolesAllowedFilter.class);
