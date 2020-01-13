fc26378fb04d4dd19247ceb249b6ee53ef1b778f
Frederik Løwe Lassen <frizz97@hotmail.com>
2020-01-13 16:15:25 +0100

    removed a endpoint

--- CA3_backend/src/main/java/rest/DemoResource.java
+++ CA3_backend/src/main/java/rest/DemoResource.java
@@ -93,24 +93,24 @@ public class DemoResource {
     return jsonStr;
   }
     
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("planet/{id}")  
-    public String getSwPlanetData(@PathParam("id") int id) throws MalformedURLException, IOException{
-    URL url = new URL("https://swapi.co/api/planets/"+id);
-    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    con.setRequestMethod("GET");
-    con.setRequestProperty("Accept", "application/json;charset=UTF-8");
-    con.setRequestProperty("User-Agent", "server"); //remember if you are using SWAPI
-    Scanner scan = new Scanner(con.getInputStream());
-    String jsonStr = null;
-    if (scan.hasNext()) {
-      jsonStr = scan.nextLine();
-      //jsonStr += "\n";
-    }
-    scan.close();
-    return jsonStr;
-  }
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @Path("planet/{id}")  
+//    public String getSwPlanetData(@PathParam("id") int id) throws MalformedURLException, IOException{
+//    URL url = new URL("https://swapi.co/api/planets/"+id);
+//    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//    con.setRequestMethod("GET");
+//    con.setRequestProperty("Accept", "application/json;charset=UTF-8");
+//    con.setRequestProperty("User-Agent", "server"); //remember if you are using SWAPI
+//    Scanner scan = new Scanner(con.getInputStream());
+//    String jsonStr = null;
+//    if (scan.hasNext()) {
+//      jsonStr = scan.nextLine();
+//      //jsonStr += "\n";
+//    }
+//    scan.close();
+//    return jsonStr;
+//  }
     
 //     @GET
 //    @Produces(MediaType.APPLICATION_JSON)
